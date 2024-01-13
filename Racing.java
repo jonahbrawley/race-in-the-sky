761edb6d5a8af1943e8ddbe67e708cb0a96b4938
jonahbrawley <jonahbrawley@gmail.com>
2024-01-13 15:01:14 -0800

    Add track and draw background

--- Racing.java
+++ Racing.java
@@ -12,7 +12,11 @@ import javax.swing.KeyStroke;
 import javax.swing.AbstractAction;
 import javax.swing.JComboBox;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.File;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -31,17 +35,23 @@ public class Racing {
 	public static void setup() {
 		appFrame = new JFrame("The Amazing Race");
 		XOFFSET = 0;
-		YOFFSET = 40;
-		WINWIDTH = 500;
-		WINHEIGHT = 500;
+		YOFFSET = 0;
+		WINWIDTH = 1000;
+		WINHEIGHT = 800;
 		pi = 3.14159265368979;
 		endgame = false;
+
+		try {
+			sunny_hill = ImageIO.read( new File("sunny_hill.png") );
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
 		setup();
 		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		appFrame.setSize(400, 400);
+		appFrame.setSize(WINWIDTH, WINHEIGHT);
 
 		JPanel myPanel = new JPanel();
 
@@ -50,6 +60,31 @@ public class Racing {
 
 		appFrame.getContentPane().add(myPanel, "Center");
 		appFrame.setVisible(true);
+		StartGame();
+	}
+
+	private static void StartGame() {
+		Thread t1 = new Thread( new Animate());
+		t1.start();
+	}
+
+	private static class Animate implements Runnable {
+		public void run() {
+			while (endgame == false) {
+				backgroundDraw();
+				try {
+					Thread.sleep(32);
+				} catch (InterruptedException e) {
+
+				}
+			}
+		}
+	}
+
+	private static void backgroundDraw() {
+		Graphics g = appFrame.getGraphics();
+		Graphics2D g2D = (Graphics2D) g;
+		g2D.drawImage(sunny_hill, XOFFSET, YOFFSET, null);
 	}
 
 	private static Boolean endgame;
@@ -62,4 +97,6 @@ public class Racing {
 	private static double pi;
 
 	private static JFrame appFrame;
+
+	private static BufferedImage sunny_hill;
 }
