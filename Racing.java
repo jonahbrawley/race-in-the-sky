5e29389f9f573df32a9adea79abec2fe19f08589
jonahbrawley <jonahbrawley@gmail.com>
2024-01-24 20:49:24 -0800

    Restructure for Swing EDT drawing

--- Racing.java
+++ Racing.java
@@ -26,17 +26,60 @@ import java.awt.*;
 public class Racing {
 	public Racing() {
 		setup();
+		initUI();
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			new Racing();
+		});
+	}
+
+	private void initUI() {
+		appFrame = new JFrame("The Amazing Race");
+		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		appFrame.setSize(WINWIDTH, WINHEIGHT);
+
+		JPanel gamePanel = new GamePanel();
+		gamePanel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
+		gbc.ipady = 15;
+		gbc.ipadx = 50;
+
+			startButton = new MyButton("START RACE");
+			startButton.addActionListener(new StartGame((GamePanel) gamePanel));
+			setButtonAppearance(startButton);
+			gamePanel.add(startButton, gbc);
+
+			gbc.insets = new Insets(10, 0, 0, 0);
+			
+			quitButton = new MyButton("QUIT");
+			quitButton.addActionListener(new QuitGame());
+			setButtonAppearance(quitButton);
+			gamePanel.add(quitButton, gbc);
+
+			bindKey((JPanel) gamePanel, "UP");
+			bindKey((JPanel) gamePanel, "DOWN");
+			bindKey((JPanel) gamePanel, "LEFT");
+			bindKey((JPanel) gamePanel, "RIGHT");
+
+		gamePanel.setBackground(CELESTIAL);
+		appFrame.getContentPane().add(gamePanel, "Center");
+		appFrame.setVisible(true);
+
+		BackgroundMusic menu_theme = new BackgroundMusic("menu.wav");
+		menu_theme.play();
 	}
 
 	public static void setup() {
-		appFrame = new JFrame("The Amazing Race");
 		XOFFSET = 0;
 		YOFFSET = 0;
 		WINWIDTH = 1000;
 		WINHEIGHT = 800;
-
 		endgame = false;
-
 		p1width = 30;
 		p1height = 30;
 		p1originalX = (double) XOFFSET + ((double) WINWIDTH / 2.15) - (p1width / 2.0);
@@ -52,112 +95,44 @@ public class Racing {
 		}
 	}
 
-	public static class BackgroundMusic implements Runnable {
-		private String file = "menu.wav";
+	// -------- BUTTON ACTIONS --------
+	private static class StartGame implements ActionListener {
+		private final GamePanel gamePanel;
 
-		public BackgroundMusic() {}
-
-		public BackgroundMusic(String file) {
-			this.file = file;
+		public StartGame(GamePanel gamePanel) {
+			this.gamePanel = gamePanel;
 		}
 
-		public void play() {
-	        Thread t = new Thread(this);
-	        t.start();
-    	}
+		public void actionPerformed(ActionEvent ae) { // autechre
+			startButton.setVisible(false);
+			quitButton.setVisible(false);
+			endgame = true;
 
-    	public void run() {
-    		playSound(file);
-    	}
+			upPressed = false;
+			downPressed = false;
+			leftPressed = false;
+			rightPressed = false;
 
-    	private void playSound(String file) {
-    		File soundFile = new File(file);
-	        AudioInputStream inputStream = null;
-			
-	        try { // get input stream
-				Clip clip = AudioSystem.getClip();
-				inputStream = AudioSystem.getAudioInputStream(soundFile);
-				clip.open(inputStream);
-				clip.loop(Clip.LOOP_CONTINUOUSLY);
-	        } 
-	        catch (Exception e) {
-	            e.printStackTrace();
-	        }
-    	}
+			p1 = new ImageObject(p1originalX, p1originalY, p1width, p1height, 0.0);
+
+			try { Thread.sleep(50); } catch (InterruptedException ie) { }
+
+			endgame = false;
+			gamePanel.startAnimation();
+			gamePanel.setGameActive(true);
+
+			Thread t1 = new Thread( new PlayerMover() );
+			t1.start();
+		}
 	}
 
-	private static void setButtonAppearance(JButton button) {
-		button.setBorder(BorderFactory.createCompoundBorder(
-			new RoundBorder(15, URANIAN),
-			BorderFactory.createEmptyBorder(10, 20, 10, 20)
-			));
-
-		button.addMouseListener(new MouseAdapter(){
-
-		    @Override
-		    public void mousePressed(MouseEvent e) {
-		        button.setBackground(HIGHLIGHT);
-		    }
-
-		    @Override
-		    public void mouseReleased(MouseEvent e) {
-		        button.setBackground(URANIAN);
-		    }
-
-		});
-
-		
-		button.setBackground(URANIAN);
-		button.setForeground(Color.BLACK);
-		button.setContentAreaFilled(false);
-		button.setOpaque(false);
-		button.setFocusPainted(false);
+	private static class QuitGame implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
 	}
 
-	private static class RoundBorder implements Border { // Used for rounded buttons
-        private int radius;
-        private Color color;
-
-        public RoundBorder(int radius, Color color) {
-            this.radius = radius;
-            this.color = color;
-        }
-
-        @Override
-        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setColor(color);
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            g2.draw(new RoundRectangle2D.Double(x, y, width - 1, height - 1, radius, radius));
-        }
-
-        @Override
-        public Insets getBorderInsets(Component c) {
-            return new Insets(radius, radius, radius, radius);
-        }
-
-        @Override
-        public boolean isBorderOpaque() {
-            return true;
-        }
-    }
-
-	private static class MyButton extends JButton {
-    	public MyButton(String text) {
-    		super(text);
-    	}
-
-    	@Override
-    	protected void paintComponent(Graphics g) {
-    		Graphics2D g2 = (Graphics2D) g.create();
-	        g2.setColor(getBackground());
-	        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));  // Adjust the radius here
-	        super.paintComponent(g);
-	        g2.dispose();
-    	}
-    }
-
+	// -------- ANIMATION AND MOVEMENT --------
 	private static class Animate implements Runnable {
 		public void run() {
 			while (!endgame) {
@@ -172,7 +147,7 @@ public class Racing {
 				g2D.drawImage(rotateImageObject(p1).filter(player1, null), (int)(p1.getX() + 0.5),
 					(int)(p1.getY() + 0.5), null);
 
-				System.out.println(p1.getX() + " " + p1.getY());
+				//System.out.println(p1.getX() + " " + p1.getY());
 
 				g2D.dispose(); // dispose old objects
 				g.dispose();
@@ -186,7 +161,47 @@ public class Racing {
 			}
 		}
 	}
-	
+
+	public static class GamePanel extends JPanel {
+		private Timer timer;
+		private boolean gameActive;
+
+		public GamePanel() {
+			timer = new Timer(32, new ActionListener() {
+				public void actionPerformed(ActionEvent evt) {
+					if (gameActive) {
+						repaint();
+					}
+				}
+			});
+
+			gameActive = false;
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if (gameActive) {
+				Graphics2D g2D = (Graphics2D) g;
+
+				g2D.drawImage(sky, XOFFSET, YOFFSET, null);
+				g2D.drawImage(dirt, XOFFSET, YOFFSET, null);
+				g2D.drawImage(track, XOFFSET, YOFFSET, null);
+
+				g2D.drawImage(rotateImageObject(p1).filter(player1, null), (int)(p1.getX() + 0.5),
+					(int)(p1.getY() + 0.5), null);
+
+				g2D.dispose();
+			}
+		}
+
+		public void startAnimation() { timer.start(); }
+
+		public void stopAnimation() { timer.stop(); }
+
+		public void setGameActive(boolean active) { gameActive = active; }
+	}
+
 	// thread responsible for updating player movement
 	private static class PlayerMover implements Runnable {
 		public PlayerMover() {
@@ -261,10 +276,155 @@ public class Racing {
 		 return (pixelColor & 0xFF000000) != 0;
 	}
 
-	// private static boolean shouldGoUnderTunnel(double carX, double carY) {
+	// initiates key actions from panel key responses
+	private static void bindKey(JPanel panel, String input) {
+		panel.getInputMap(IFW).put(KeyStroke.getKeyStroke("pressed " + input), input + " pressed");
+		panel.getActionMap().put(input + " pressed", new KeyPressed(input));
 
-	// }
+		panel.getInputMap(IFW).put(KeyStroke.getKeyStroke("released " + input), input + " released");
+		panel.getActionMap().put(input + " released", new KeyReleased(input));
+	}
+
+	// monitors keypresses
+	private static class KeyPressed extends AbstractAction {
+		public KeyPressed() { action = ""; }
+
+		public KeyPressed(String input) { action = input; }
+
+		public void actionPerformed(ActionEvent e) {
+			if (action.equals("UP")) { upPressed = true; }
+			if (action.equals("DOWN")) { downPressed = true; }
+			if (action.equals("LEFT")) { leftPressed = true; }
+			if (action.equals("RIGHT")) { rightPressed = true; }
+		}
 	
+		private String action;
+	}
+
+	// monitors keyreleases
+	private static class KeyReleased extends AbstractAction {
+		public KeyReleased() { action = ""; }
+
+		public KeyReleased(String input) { action = input; }
+
+		public void actionPerformed(ActionEvent e) {
+			if (action.equals("UP")) { upPressed = false; }
+			if (action.equals("DOWN")) { downPressed = false; }
+			if (action.equals("LEFT")) { leftPressed = false; }
+			if (action.equals("RIGHT")) { rightPressed = false; }
+		}
+
+		private String action;
+	}
+
+	// -------- UI AND APPEARANCE --------
+	public static class BackgroundMusic implements Runnable {
+		private String file = "menu.wav";
+
+		public BackgroundMusic() {}
+
+		public BackgroundMusic(String file) {
+			this.file = file;
+		}
+
+		public void play() {
+	        Thread t = new Thread(this);
+	        t.start();
+    	}
+
+    	public void run() {
+    		playSound(file);
+    	}
+
+    	private void playSound(String file) {
+    		File soundFile = new File(file);
+	        AudioInputStream inputStream = null;
+			
+	        try { // get input stream
+				Clip clip = AudioSystem.getClip();
+				inputStream = AudioSystem.getAudioInputStream(soundFile);
+				clip.open(inputStream);
+				clip.loop(Clip.LOOP_CONTINUOUSLY);
+	        } 
+	        catch (Exception e) {
+	            e.printStackTrace();
+	        }
+    	}
+	}
+
+	private static class MyButton extends JButton {
+    	public MyButton(String text) {
+    		super(text);
+    	}
+
+    	@Override
+    	protected void paintComponent(Graphics g) {
+    		Graphics2D g2 = (Graphics2D) g.create();
+	        g2.setColor(getBackground());
+	        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth() - 1, getHeight() - 1, 15, 15));  // Adjust the radius here
+	        super.paintComponent(g);
+	        g2.dispose();
+    	}
+    }
+
+	private static void setButtonAppearance(JButton button) {
+		button.setBorder(BorderFactory.createCompoundBorder(
+			new RoundBorder(15, URANIAN),
+			BorderFactory.createEmptyBorder(10, 20, 10, 20)
+			));
+
+		button.addMouseListener(new MouseAdapter(){
+
+		    @Override
+		    public void mousePressed(MouseEvent e) {
+		        button.setBackground(HIGHLIGHT);
+		    }
+
+		    @Override
+		    public void mouseReleased(MouseEvent e) {
+		        button.setBackground(URANIAN);
+		    }
+
+		});
+
+		
+		button.setBackground(URANIAN);
+		button.setForeground(Color.BLACK);
+		button.setContentAreaFilled(false);
+		button.setOpaque(false);
+		button.setFocusPainted(false);
+	}
+
+	private static class RoundBorder implements Border { // Used for rounded buttons
+        private int radius;
+        private Color color;
+
+        public RoundBorder(int radius, Color color) {
+            this.radius = radius;
+            this.color = color;
+        }
+
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(color);
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2.draw(new RoundRectangle2D.Double(x, y, width - 1, height - 1, radius, radius));
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(radius, radius, radius, radius);
+        }
+
+        @Override
+        public boolean isBorderOpaque() {
+            return true;
+        }
+    }
+
+    // -------- UTILITY FUNCTIONS --------
 	// moveable image objects
 	private static class ImageObject {
 		private double x, y, xwidth, yheight, angle, internalangle, comX, comY;
@@ -402,127 +562,9 @@ public class Racing {
 		return atop;
 	}
 
-	// initiates key actions from panel key responses
-	private static void bindKey(JPanel panel, String input) {
-		panel.getInputMap(IFW).put(KeyStroke.getKeyStroke("pressed " + input), input + " pressed");
-		panel.getActionMap().put(input + " pressed", new KeyPressed(input));
-
-		panel.getInputMap(IFW).put(KeyStroke.getKeyStroke("released " + input), input + " released");
-		panel.getActionMap().put(input + " released", new KeyReleased(input));
-	}
-
-	// monitors keypresses
-	private static class KeyPressed extends AbstractAction {
-		public KeyPressed() { action = ""; }
-
-		public KeyPressed(String input) { action = input; }
-
-		public void actionPerformed(ActionEvent e) {
-			if (action.equals("UP")) { upPressed = true; }
-			if (action.equals("DOWN")) { downPressed = true; }
-			if (action.equals("LEFT")) { leftPressed = true; }
-			if (action.equals("RIGHT")) { rightPressed = true; }
-		}
-	
-		private String action;
-	}
-
-	// monitors keyreleases
-	private static class KeyReleased extends AbstractAction {
-		public KeyReleased() { action = ""; }
-
-		public KeyReleased(String input) { action = input; }
-
-		public void actionPerformed(ActionEvent e) {
-			if (action.equals("UP")) { upPressed = false; }
-			if (action.equals("DOWN")) { downPressed = false; }
-			if (action.equals("LEFT")) { leftPressed = false; }
-			if (action.equals("RIGHT")) { rightPressed = false; }
-		}
-
-		private String action;
-	}
-
-	private static class StartGame implements ActionListener {
-		private final JPanel panel;
-
-		public StartGame(JPanel panel) {
-			this.panel = panel;
-		}
-
-		public void actionPerformed(ActionEvent ae) {
-			startButton.setVisible(false);
-			quitButton.setVisible(false);
-			endgame = true;
-
-			upPressed = false;
-			downPressed = false;
-			leftPressed = false;
-			rightPressed = false;
-
-			p1 = new ImageObject(p1originalX, p1originalY, p1width, p1height, 0.0);
-			p1velocity = 0.0;
-
-			try { Thread.sleep(50); } catch (InterruptedException ie) { }
-
-			endgame = false;
-			Thread t1 = new Thread( new Animate() );
-			Thread t2 = new Thread( new PlayerMover() );
-			t1.start();
-			t2.start();
-		}
-	}
-
-	private static class QuitGame implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			System.exit(0);
-		}
-	}
-
-	public static void main(String[] args) {
-		setup();
-		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		appFrame.setSize(WINWIDTH, WINHEIGHT);
-
-		JPanel myPanel = new JPanel();
-		myPanel.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-
-		gbc.ipady = 15;
-		gbc.ipadx = 50;
-
-			startButton = new MyButton("START RACE");
-			startButton.addActionListener(new StartGame(myPanel));
-			setButtonAppearance(startButton);
-			myPanel.add(startButton, gbc);
-
-			gbc.insets = new Insets(10, 0, 0, 0);
-			
-			quitButton = new MyButton("QUIT");
-			quitButton.addActionListener(new QuitGame());
-			setButtonAppearance(quitButton);
-			myPanel.add(quitButton, gbc);
-
-			bindKey(myPanel, "UP");
-			bindKey(myPanel, "DOWN");
-			bindKey(myPanel, "LEFT");
-			bindKey(myPanel, "RIGHT");
-
-		myPanel.setBackground(CELESTIAL);
-		appFrame.getContentPane().add(myPanel, "Center");
-		appFrame.setVisible(true);
-		appFrame.createBufferStrategy(2);
-		bs = appFrame.getBufferStrategy();
-		
-		BackgroundMusic menu_theme = new BackgroundMusic("menu.wav");
-		menu_theme.play();
-	}
-
+	// -------- GLOBAL VARIABLES --------
 	private static Boolean endgame;
 	private static Boolean upPressed, downPressed, leftPressed, rightPressed;
-	private static Boolean tunnelCover;
 
 	private static JButton startButton, quitButton;
 
